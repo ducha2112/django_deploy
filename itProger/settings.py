cf6c01c3dc786623eb9a8c625a246ba500d3baa9
ducha2112 <ducha2112@yandex.ru>
2023-02-20 21:10:23 +0300

    'deploy'

--- itProger/settings.py
+++ itProger/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()a0ion(4i6ge*985gk*zaxz-8$imob7wrv(u=#9taa7yas(=p'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG',False)
 
 ALLOWED_HOSTS = []
 
@@ -125,13 +127,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ducha2112@gmail.com'
-EMAIL_HOST_PASSWORD = 'cfxdosqzwdoswcwk'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # login = 'admin'
 # password = 'adminlkjhgfdsa'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'var/www/ducha2112/static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
